71e28138be1bb279529bb08ee72891f0666e9d39
Harshal Honde <105191619+harshalhonde21@users.noreply.github.com>
2024-08-04 11:42:13 +0530

    updated

---
+++ Backend/Day1/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+app.use(express.json());
+
+
+const port = 5500;
+const mongoUrl = "mongodb://127.0.0.1/sampleServer";
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => app.listen(port))
+  .then(() =>
+    console.log(`⚙️  Server is running and connected to db at port ${port} :)`)
+  )
+  .catch((err) => console.log(`${err} is error`));
--- JavaScript-Basics/Day7/script.js
+++ JavaScript-Basics/Day7/script.js
@@ -1,31 +1,50 @@
 document.getElementById('generate').addEventListener('click', generatePassword);
-document.getElementById('length').addEventListener('input', updateLengthValue);
+
+document.getElementById('length').addEventListener('input', updateLengthValue)
+
 document.getElementById('copy').addEventListener('click', copyPassword);
 
-function generatePassword() {
-  const length = document.getElementById('length').value;
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
-  let password = '';
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
-  const passwordInput = document.getElementById('password');
-  passwordInput.value = password;
-  passwordInput.style.width = `${length * 11}px`; 
+
+
+function generatePassword(){
+    const length = document.getElementById('length').value;
+
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    let password = '';
+
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex]
+    }
+
+    const passwordInput = document.getElementById('password');
+
+    passwordInput.value = password;
+
+    passwordInput.style.width = `${length * 10}px`;
 }
 
-function updateLengthValue() {
-  const lengthValue = document.getElementById('length').value;
-  document.getElementById('length-value').textContent = lengthValue;
+function updateLengthValue(){
+    const lengthValue = document.getElementById('length').value;
+
+    document.getElementById('length-value').textContent = lengthValue
 }
 
-function copyPassword() {
-  const passwordInput = document.getElementById('password');
-  passwordInput.select();
-  document.execCommand('copy');
-  alert('Password copied to clipboard!');
+function copyPassword(){
+    const passwordInput = document.getElementById('password');
+    passwordInput.select();
+    document.execCommand('copy');
+    alert('Password Copied Success');
 }
+
+
+
+
+
+
 
 
 // execCommand
+// ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=
