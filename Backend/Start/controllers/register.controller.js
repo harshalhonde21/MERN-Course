c41a90096a245a40d0ecfd64185c18be5540c532
Harshal Honde <105191619+harshalhonde21@users.noreply.github.com>
2024-08-18 22:17:06 +0530

    updated with update nd delete api

--- Backend/Start/controllers/register.controller.js
+++ Backend/Start/controllers/register.controller.js
@@ -1,5 +1,5 @@
 import Student from "../models/register.model.js";
-import cloudinary from "../config/cloudinary.js"
+import cloudinary from "../config/cloudinary.js";
 
 export const registerStudent = async (req, res) => {
     try {
@@ -15,8 +15,8 @@ export const registerStudent = async (req, res) => {
         }
 
         let imageUrl = null;
-        if(req.file){
-            const result  = await cloudinary.uploader.upload(req.file.path)
+        if (req.file) {
+            const result = await cloudinary.uploader.upload(req.file.path);
             imageUrl = result.secure_url;
         }
 
@@ -27,7 +27,7 @@ export const registerStudent = async (req, res) => {
             marks,
             address,
             phone,
-            image: imageUrl
+            image: imageUrl,
         });
 
         res.status(201).json({
@@ -66,3 +66,92 @@ export const getStudents = async (req, res) => {
         });
     }
 };
+
+export const updateStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, age, marks, address, phone } = req.body;
+
+        let student = await Student.findById(id);
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found",
+            });
+        }
+
+        let imageUrl = student.image;
+
+        if (req.file) {
+            if (imageUrl) {
+                const publicId = imageUrl.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(publicId);
+            }
+        }
+
+        const result = await cloudinary.uploader.upload(req.file.path);
+        imageUrl = result.secure_url;
+
+        student = await Student.findByIdAndUpdate(
+            id,
+            {
+                name,
+                email,
+                age,
+                marks,
+                address,
+                phone,
+                image: imageUrl,
+            },
+            { new: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Student updated successfully",
+            student,
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
+export const deleteStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const student = await Student.findById(id);
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found",
+            });
+        }
+
+        let imageUrl = student.image;
+
+            if (imageUrl) {
+                const publicId = imageUrl.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(publicId);
+            }
+
+        await Student.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success: true,
+            message: "Student deleted successfully",
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
--- Backend/Start/routes/register.route.js
+++ Backend/Start/routes/register.route.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { getStudents, registerStudent } from "../controllers/register.controller.js";
+import { deleteStudent, getStudents, registerStudent, updateStudent } from "../controllers/register.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer-config.js"
 
 const router = express.Router();
 
 router.route("/student-register").post(upload.single("image"), registerStudent);
-router.route("/get-students").get(isAuthenticated, getStudents);
+router.route("/get-students").get(getStudents);
+router.route("/update-student/:id").put(upload.single("image"), updateStudent);
+router.route("/delete-student/:id").delete(deleteStudent)
 
 
 export default router;
